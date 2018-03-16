8fc46c8c81d1178e42098dfebadf73f3660f8086
Jordan Skophammer <skop22@Jordan-Skophammers-iMac.local>
2018-03-16 06:53:47 -0500

    added javascript answers and image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,51 @@
 $(document).ready(function(){
-	var questionOne = "How many islands make up Japan?";
-	var questionTwo = "";
-	var questionThree = "";
-	var questionFour = "";
-	var questionFive = "";
+	// var questionOne = "How many islands make up Japan?";
+	// var questionTwo = "";
+	// var questionThree = "";
+	// var questionFour = "";
+	// var questionFive = "";
 
+	function setTimer(){
+		var i = 30;
+		var countDown = setInterval(function() {
+		    $('#timer').text(i);
 
-	var i = 30;
-	var countDown = setInterval(function() {
-	    document.getElementById('timer').innerHTML = i;
+		    if (i === 0) {
+		        clearInterval(countDown);
+		        $('#timer').text("TIME'S UP!");
+		    }
+		    else {
+		        i--;
+		    }
+		}, 1000)
+	};
 
-	    if (i === 0) {
-	        clearInterval(countDown);
-	        document.getElementById('timer').innerHTML = "TIME'S UP!"
-	    }
-	    else {
-	        i--;
-	    }
-	}, 1000)
+	setTimer();
 
+	function questionOne() {
 
-	// countDown();
+		var answersOne = {
+			A : "4",
+			B : "33",
+			C : "108",
+			D : "6852"
+		};
 
-	// var answersOne = {
-	// 	A : "4",
-	// 	B : "33",
-	// 	C : "108",
-	// 	D : "6852"
-	// };
+		var questionOneAnswer = answersOne.D;
+
+		$('#questionBody').text('How many islands make up Japan?');
+		$('#A').text(answersOne.A);
+		$('#B').text(answersOne.B);
+		$('#C').text(answersOne.C);
+		$('#D').text(answersOne.D);
+		$('#questionImage').attr('src', './assets/images/japan_map_base.png')
+
+	}
+
+	questionOne();
+	
 
 	
-	document.getElementById('questionBody').innerHTML = "How many islands make up Japan?";
 });
 
 
